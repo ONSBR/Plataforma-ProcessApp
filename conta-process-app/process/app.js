5000c9b4035b40b3d171a6bacd1b703c329e1d21
Hugo Menezes <hugo.menezes@pitang.com>
2017-12-05 18:15:18 -0300

    gravar dados da memoria do processo

--- conta-process-app/process/app.js
+++ conta-process-app/process/app.js
@@ -1,7 +1,11 @@
 var { ProcessMemory } = require('./ProcessMemory');
 var Contexto = require('plataforma-core/Contexto');
+var Evento = require('plataforma-core/Evento');
+var Instancia = require('plataforma-core/Instancia');
 
-var contexto = new Contexto("", "", "", Date.now());
+var instancia = new Instancia("1");
+var evento = new Evento("cadastro", Date.now(), instancia, "usuario 1", "payload");
+var contexto = new Contexto(evento, instancia, "", Date.now());
 
 ProcessMemory.writeFileSync(1, JSON.stringify(contexto));
 // var json = ReadProcessMemory.readFileSync(process.argv[2]);
