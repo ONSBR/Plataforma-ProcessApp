3161c94579c67864a905a92253ac1f956fe66cf7
Hugo Menezes <hugomenezes@gmail.com>
2017-12-11 17:40:40 -0300

    gerar js

--- conta-process-app/process/app.js
+++ conta-process-app/process/app.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var yaml = require("js-yaml");
+var handlebars = require('handlebars');
 
 let nomeDoYaml = process.argv[2];
 geraEntidade(nomeDoYaml);
@@ -8,14 +9,24 @@ function geraEntidade(nomeDoYaml) {
     console.log("Gerando entidade a partir do yaml=" + nomeDoYaml);
     let yamlFile = lerYaml(nomeDoYaml);
     let yamlMapper = yaml.safeLoad(yamlFile);
-    
     var campos = eval("yamlMapper."+ nomeDoYaml + ".fields");
-    
-    console.log("nome da entidade " + nomeDoYaml);
-    console.log("nome dos campos=")
-    for (campo in campos) {
-        console.log(campo)
+    geraJs(nomeDoYaml, campos);
+}
+
+function geraJs(nomeDoYaml, campos) {
+    var source = fs.readFileSync("dominio/dominio.tmpl").toString();
+    var template = handlebars.compile(source);
+    var objTemplate = { "nomeDaClasse":nomeDoYaml, "campos":getListaDeCampos(campos)}; 
+    var compiled = template(objTemplate);
+    console.log(compiled);
+}
+
+function getListaDeCampos(campos) {
+    var lista = [];
+     for (campo in campos) {
+        lista.push({campo: campo});
     }
+    return lista;
 }
 
 function lerYaml(nomeDoYaml) {
