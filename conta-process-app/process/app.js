415aa347bfa640600fb03b125156f4485f9c5389
Hugo Menezes <hugomenezes@gmail.com>
2017-12-11 15:59:20 -0300

    gerando entidade a partir do yaml

--- conta-process-app/process/app.js
+++ conta-process-app/process/app.js
@@ -2,26 +2,23 @@ var fs = require("fs");
 var yaml = require("js-yaml");
 
 let nomeDoYaml = process.argv[2];
-console.log("Gerando entidade a partir do yaml=" + nomeDoYaml);
-let yamlFile = lerYaml(nomeDoYaml);
-let yamlMapper = yaml.safeLoad(yamlFile);
+geraEntidade(nomeDoYaml);
 
-var campos = eval("yamlMapper."+ nomeDoYaml + ".fields");
-
-console.log("nome da entidade " + nomeDoYaml);
-console.log("nome dos campos=")
-for (const campo in campos) {
-    console.log(campo)
+function geraEntidade(nomeDoYaml) {
+    console.log("Gerando entidade a partir do yaml=" + nomeDoYaml);
+    let yamlFile = lerYaml(nomeDoYaml);
+    let yamlMapper = yaml.safeLoad(yamlFile);
+    
+    var campos = eval("yamlMapper."+ nomeDoYaml + ".fields");
+    
+    console.log("nome da entidade " + nomeDoYaml);
+    console.log("nome dos campos=")
+    for (campo in campos) {
+        console.log(campo)
+    }
 }
 
 function lerYaml(nomeDoYaml) {
     let yamlFile = fs.readFileSync("mapa/"+ nomeDoYaml +".yaml");
-    console.log("===========");
-    console.log(yamlFile.toString());
-    console.log("===========");
     return yamlFile;
-}
-
-function createJsClass() {
-    
 }
