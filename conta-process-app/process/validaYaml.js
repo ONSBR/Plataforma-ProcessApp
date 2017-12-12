bc216d4b80c548725c5dc0f3467c4abffaea3535
Hugo Menezes <hugomenezes@gmail.com>
2017-12-12 16:21:57 -0300

    metadado da operacao

--- conta-process-app/process/validaYaml.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-var yaml = require("js-yaml");
-
-operacaoFile = fs.readFileSync("../operacoes/operacao.yaml");
-var operacaoYaml = yaml.safeLoad(operacaoFile);
-console.log(operacaoYaml.operacao)
