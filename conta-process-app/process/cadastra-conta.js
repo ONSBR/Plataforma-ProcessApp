4782a13f5f2b1cc1cf5a947d2fd6182be491ff9b
Hugo Menezes <hugomenezes@gmail.com>
2017-12-13 14:48:27 -0300

    preenchendo data do contexto

--- conta-process-app/process/cadastra-conta.js
+++ conta-process-app/process/cadastra-conta.js
@@ -1,5 +1,8 @@
+var config = require('config');
+
 function insereConta(contexto) {
     console.log(contexto);
+    contexto.dataSet.save(contexto.evento.payload);
 }
 
 module.exports.insereConta = insereConta
