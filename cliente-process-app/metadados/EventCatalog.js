0b735bfbc3d063c098b02ffbf43ac654b3742304
INTRANET\fca <fernando.andrade@pitang.com>
2017-12-26 08:07:13 -0300

    confirmados eventos para cada transação

--- cliente-process-app/metadados/EventCatalog.js
+++ cliente-process-app/metadados/EventCatalog.js
@@ -1,4 +1,8 @@
 module.exports = {
     client_put: "client.put",
-    client_saved: "client.saved"
+    client_saved: "client.saved",
+
+    transfer_request: "transfer.request",
+    transfer_confirmation: "transfer.confirmation"
+
 }
---
+++ transferencia-process-app/process/transferencia-conta.js
@@ -0,0 +1,34 @@
+var Evento = require("../../../Plataforma-core/Evento");
+var EventCatalog = require("../metadados/EventCatalog");
+
+function transfereConta(contexto) {
+    
+    var contaOrigem = contexto.evento.payload.contaOrigem;
+    var contaDestino = contexto.evento.payload.contaDestino;
+    var valorTransferencia = contexto.evento.payload.operacao.valorTransferencia;
+    
+    contaOrigem.saldo = contaOrigem.saldo - valorTransferencia;
+    contaDestino.saldo = contaDestino.saldo + valorTransferencia
+    
+    contexto.evento.payload.contaOrigem = contaOrigem;
+    contexto.evento.payload.contaDestino = contaDestino;
+
+    contexto.dataSet.save(getTransfer(contexto.evento.payload), "Transfer");
+    console.log(contexto.evento.payload);
+
+    contexto.eventoSaida = new Evento(
+        EventCatalog.transfer_confirmation, 
+        contexto.evento.processName,
+        new Date(), 
+        contexto.instancia, null, 
+        contexto.evento.payload, 
+        contexto.evento.origem
+    );
+}
+
+function getTransfer(account) {
+    return '[{ "saldo":' + account.saldo + ', "_metadata": { "type": "conta", "changeTrack": "create" } }]';
+}
+
+
+module.exports.transfereConta = transfereConta
