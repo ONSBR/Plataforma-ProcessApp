ca6aa5f75cd65ead6042b598187b3c7f41341043
Hugo Menezes <hugomenezes@gmail.com>
2018-01-03 16:09:04 -0300

    cadastro de clientes

--- cliente-process-app/process/cadastra-cliente.js
+++ cliente-process-app/process/cadastra-cliente.js
@@ -2,9 +2,7 @@ var Evento = require("plataforma-core/Evento");
 var EventCatalog = require("../metadados/EventCatalog");
 
 function insereCliente(contexto) {
-    //FIXME Criar classe Cliente
     contexto.dataSet.save(getClient(contexto.evento.payload), "Client");
-    console.log(contexto.evento.payload);
 
     contexto.eventoSaida = new Evento(
         EventCatalog.account_saved, 
@@ -16,9 +14,8 @@ function insereCliente(contexto) {
     );
 }
 
-function getClient(account) {
-    return '[{ "nome":' + account.titular + ', "_metadata": { "type": "cliente", "changeTrack": "create" } }]';
+function getClient(cliente) {
+    return '[{ "nome":' + cliente.titular + ', "_metadata": { "type": "cliente", "changeTrack": "create" } }]';
 }
-
 
 module.exports.insereCliente = insereCliente
